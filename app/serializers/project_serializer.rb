3caa14826d25056836193e723da21162485bd222
seanliu93 <sean93liu@gmail.com>
2016-06-14 17:02:40 -0400

    added dynamic response to project funds button

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -2,12 +2,7 @@ class ProjectSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :user_id, :fund_goal, :end_date
   has_many :funds
   has_many :categories
-  has_many :backers
   class FundSerializer < ActiveModel::Serializer
-  	attributes :id, :backer_id, :amount
-  end
-
-  class User < ActiveModel::Serializer
-    attributes :id, :email
+  	attributes :id, :amount, :backer
   end
 end
